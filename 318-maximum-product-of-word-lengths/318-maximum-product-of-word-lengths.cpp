57154b4201bac4ef6e5d9ba63976b89283ef178b
desouky22 <63201035+desouky22@users.noreply.github.com>
2022-05-29 13:37:43 +0200

    Time: 740 ms (12.34%), Space: 17.6 MB (33.29%) - LeetHub

--- 318-maximum-product-of-word-lengths/318-maximum-product-of-word-lengths.cpp
+++ 318-maximum-product-of-word-lengths/318-maximum-product-of-word-lengths.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
-    map<string,int>mp;
-    bool valid(string a , string b){
+    map<int,int>mp;
+    bool valid(int a , int b){
         return ((mp[a] & mp[b]) == 0);
     }
     
     int maxProduct(vector<string>& words) {
         for(int x = 0; x<words.size(); x++){
             for(auto it: words[x]){
-                mp[words[x]] = mp[words[x]] | (1 << (it - 'a'));
+                mp[x] = mp[x] | (1 << (it - 'a'));
             }
         }
         
         long long mx = 0;
-        for(int x = 0; x<words.size()-1; x++){
+        for(int x = 0; x<words.size(); x++){
             for(int y = x+1; y<words.size(); y++){
-                if(valid(words[x], words[y]))
+                if(valid(x, y))
                     mx = max(mx, (long long)words[x].size()*(long long)words[y].size());
             }
         }
