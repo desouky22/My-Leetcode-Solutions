315671e5a5f81c172f229218b2555e80873928ff
desouky22 <63201035+desouky22@users.noreply.github.com>
2022-05-30 23:28:37 +0200

    Time: 5 ms (96.45%), Space: 7.9 MB (94.28%) - LeetHub

---
+++ 657-robot-return-to-origin/657-robot-return-to-origin.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool judgeCircle(string moves) {
+        int x = 0, y = 0;
+        for(auto it : moves){
+            if(it == 'R')x++;
+            else if(it == 'L')x--;
+            else if(it == 'U')y++;
+            else y--;
+        }
+        return x == 0 and y == 0;
+    }
+};
