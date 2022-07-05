7302285ebac900bceae55d9fcf09bbd2318b3a89
desouky22 <63201035+desouky22@users.noreply.github.com>
2022-07-05 03:41:22 +0200

    Time: 19 ms (28.46%), Space: 10.4 MB (5.95%) - LeetHub

---
+++ 64-minimum-path-sum/64-minimum-path-sum.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    long long n , m;
+    vector<vector<int>>v;
+    int mem[250][250];
+    int solve(int row = 0, int col = 0){
+        if(row == n-1 and col == m-1)return v[row][col];
+        if(mem[row][col] != -1)return mem[row][col];
+        long long path1 = INT_MAX , path2 = INT_MAX;
+        int ret = INT_MAX;
+        if(row+1 < n) ret = min(ret, v[row][col] + solve(row+1 , col));
+        if(col+1 < m) ret = min(ret, v[row][col] + solve(row , col+1));
+        return mem[row][col] = ret;
+    }
+    
+    
+    int minPathSum(vector<vector<int>>& grid) {
+        n = grid.size();
+        m = grid[0].size();
+        v = grid;
+        memset(mem, -1, sizeof mem);    
+        return solve();
+    }
+};
