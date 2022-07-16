04432bb2cceec7a656ba30ce573f80b9fc47ee3c
desouky22 <63201035+desouky22@users.noreply.github.com>
2022-07-16 15:46:09 +0200

    Time: 9 ms (57.59%), Space: 9.1 MB (7.91%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -1,18 +1,21 @@
 class Solution {
 public:
-    vector<vector<int>>v;
-    int dp[205][200];
+    vector<vector<int>> triangle;
+    int n;
+    int mem[201][201];
     int solve(int row, int col){
-        if(row == v.size()) return 0;
-        if(dp[row][col] != -1)return dp[row][col];
-        int path1 = solve(row+1, col+1) + v[row][col];
-        int path2 = solve(row+1, col) + v[row][col];
-        return dp[row][col] = min(path1 , path2);
+        if(row == n) return 0;
+        if(mem[row][col] != -1) return mem[row][col];
+        int path1 = INT_MAX , path2 = INT_MAX;
+        path1 = triangle[row][col] + solve(row+1, col);
+        path2 = triangle[row][col] + solve(row+1, col+1);
+        return mem[row][col] = min(path1 , path2);
     }
     
-    int minimumTotal(vector<vector<int>>& triangle) {
-        v = triangle;
-        memset(dp , -1, sizeof dp);
+    int minimumTotal(vector<vector<int>>& t) {
+        triangle = t;
+        n = t.size(); 
+        memset(mem, -1, sizeof mem);
         return solve(0,0);
     }
 };
