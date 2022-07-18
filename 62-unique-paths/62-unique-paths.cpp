1d61f41cee2cc06c447e4ae4c30336c43c062e8a
desouky22 <63201035+desouky22@users.noreply.github.com>
2022-07-18 15:17:13 +0200

    Time: 0 ms (100.00%), Space: 6 MB (61.31%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -1,18 +1,21 @@
 class Solution {
 public:
-    int n , m;
-    int mem[200][200];
-    int solve(int row=0, int col=0){
-        if(row == n-1 and col == m-1)return 1;
-        if(mem[row][col] != -1)return mem[row][col];
-        int path1 = 0 , path2 = 0;
-        if(row+1 < n) path1 = solve(row+1 , col);
-        if(col+1 < m) path2 = solve(row, col+1);
-        return mem[row][col] = path1 + path2;
+    int n, m;
+    int mem[109][109];
+    bool valid(int row, int col){
+        return row>=0 and row<n and col>=0 and col<m;
     }
-    int uniquePaths(int mm, int nn) {
-        n = mm;
-        m = nn;
+    int solve(int row = 0, int col = 0){
+        if(row == n-1 and col == m-1)return 1;
+        if(!valid(row, col))return 0;
+        if(mem[row][col] != -1)return mem[row][col];
+        int goRight = solve(row, col+1);
+        int goDown = solve(row+1, col);
+        return mem[row][col] = goRight + goDown;
+    }
+    int uniquePaths(int N, int M) {
+        n = N;
+        m = M;
         memset(mem, -1, sizeof mem);
         return solve();
     }
