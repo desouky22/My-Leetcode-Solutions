fd15a015f836bf698f3eaa41652c32b9d7632fea
desouky22 <63201035+desouky22@users.noreply.github.com>
2022-07-24 09:04:02 +0200

    Time: 206 ms (7.23%), Space: 8.7 MB (67.23%) - LeetHub

--- 72-edit-distance/72-edit-distance.cpp
+++ 72-edit-distance/72-edit-distance.cpp
@@ -1,24 +1,25 @@
 class Solution {
 public:
-    int n , m;
+    typedef long long ll;
     string a, b;
-    int mem[1000][1000];
-    int solve(int idx1 = 0, int idx2 = 0){
-        if(idx1 == n or idx2 == m)return abs(n-idx1) + abs(m-idx2);
-        if(mem[idx1][idx2] != -1)return mem[idx1][idx2];
-        int path1 = INT_MAX , path2 = INT_MAX , path3 = INT_MAX , path4 = INT_MAX;
-        if(a[idx1] == b[idx2]) path1 = solve(idx1 + 1 , idx2 + 1);
-        if(a[idx1] != b[idx2]) path2 = 1 + solve(idx1 + 1 , idx2 + 1);
-        path3 = 1 + solve(idx1 + 1 , idx2);
-        path4 = 1 + solve(idx1 , idx2 + 1);
-        return mem[idx1][idx2] = min({path1 , path2 , path3 , path4});
+    int n ,m;
+    ll mem[509][509];
+    ll solve(int idx1 = 0, int idx2 = 0){
+        if(idx2 == m) return n - idx1;
+        if(idx1 == n) return m - idx2;
+        if(mem[idx1][idx2] != -1) return mem[idx1][idx2];
+        if(a[idx1] == b[idx2])return mem[idx1][idx2] = solve(idx1 + 1 , idx2 + 1);
+        ll path1 = 1 + solve(idx1 + 1 , idx2);
+        ll path2 = 1 + solve(idx1, idx2 + 1);
+        ll path3 = 1 + solve(idx1+1, idx2+1);
+        return mem[idx1][idx2] = min({path1 , path2, path3});
     }
     int minDistance(string word1, string word2) {
-        a = word1;
-        b = word2;
-        n = a.size();
-        m = b.size();
+        a = word1, b = word2;
+        n = a.size() , m = b.size();
         memset(mem, -1, sizeof mem);
         return solve();
     }
 };
+
+
