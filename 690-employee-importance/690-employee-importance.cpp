d6eadaf92a8f502415f72b72e051be2002b6cb90
desouky22 <63201035+desouky22@users.noreply.github.com>
2022-07-27 05:30:18 +0200

    Time: 59 ms (37.19%), Space: 13.2 MB (97.77%) - LeetHub

--- 690-employee-importance/690-employee-importance.cpp
+++ 690-employee-importance/690-employee-importance.cpp
@@ -7,11 +7,8 @@ public:
     vector<int> subordinates;
 };
 */
-vector<bool>vis;
 vector<Employee*> emps;
-
 int dfs(int id){
-    vis[id] = true;
     Employee* emp;
     for(auto it : emps){
         if(it->id == id){
@@ -31,7 +28,6 @@ class Solution {
 public:
     int getImportance(vector<Employee*> employees, int id) {
         emps = employees;
-        vis.resize(3000, false);
         return dfs(id);
         return 0;
     }
